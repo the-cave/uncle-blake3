6c8158fd7a80c721228b75db019a18d8319b71a0
Sarun Rattanasiri <midnight_w@gmx.tw>
2022-02-17 09:38:55 +0700

    new gem

---
+++ lib/uncle_blake3.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "uncle_blake3/version"
+
+module UncleBlake3
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/uncle_blake3/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module UncleBlake3
+  VERSION = "0.1.0"
+end
